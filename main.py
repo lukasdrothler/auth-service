9fdf5c72210d35a0a6d057c867e5432046ac574b
ImpulZzZ <lukas.drothler@web.de>
2025-11-15 13:35:44 +0100

    fix: enable hot reload for development environment in main application run configuration

--- main.py
+++ main.py
@@ -66,4 +66,4 @@ app.include_router(stripe.router)
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host=os.getenv("AUTH_SVC_HOST", "0.0.0.0"), port=int(os.getenv("AUTH_SVC_PORT", 8007)))
+    uvicorn.run("main:app", host=os.getenv("AUTH_SVC_HOST", "0.0.0.0"), port=int(os.getenv("AUTH_SVC_PORT", 8007)), reload=hot_reload)
